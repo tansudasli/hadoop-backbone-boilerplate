0334a8e3261ef644e694add7741b79100c4459ea
Tansu Dasli <tansudasli@gmail.com>
2020-03-21 00:28:09 +0300

    Initial ZK configs

---
+++ playbook-zookeeper/configure-zookeeper.sh
@@ -0,0 +1,26 @@
+
+source .env
+
+# contains zk specific env variables !
+
+echo "run on nameNode only"
+#
+#
+
+
+# hadoop arch. topology - fully distributed
+# export NAME_NODE=${INSTANCE_NAMES[0]}
+# export SECONDARY_NAME_NODE=${INSTANCE_NAMES[1]}
+# export WORKER_NODES=(${INSTANCE_NAMES[2]} ${INSTANCE_NAMES[3]})
+export ZK_PATH=(/data-1)
+
+echo "configurations of ZK"
+
+
+# creates as new file
+# public-IP creates bindingException error
+cat > ${ZOOKEEPER_HOME}/zoo.cfg <<EOL
+tickTime=2000
+dataDir=${ZK_PATH}/zookeeper
+clientPort=2181
+EOL
