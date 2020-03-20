4b2f53ae39f35038fdbd77d5ab4432105a1db8fd
Tansu Dasli <tansudasli@gmail.com>
2020-03-20 17:04:16 +0300

    added more parameters

--- 01-preparations.sh
+++ 01-preparations.sh
@@ -2,12 +2,11 @@
 
 source .env
 
-# ----------- preperations ---------
-
 echo "First, create a gcp account and install gcp cli on your local"
 echo "Then, check active gcp account"
+echo "Then, edit .env file"
 echo "Then, run this script to create a project on gcp"
 
-gcloud projects create ${PROJECT_ID} --name 'hadoop-cluster'
+gcloud projects create ${PROJECT_ID} --name ${PROJECT_NAME}
 
-gcloud alpha billing projects link ${PROJECT_ID} --billing-account 00F27E-4EE4CA-3797DD
+gcloud alpha billing projects link ${PROJECT_ID} --billing-account ${BILLING_ACCOUNT}
