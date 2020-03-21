9f95ccc3931ae188be17da1fdd70e4f9ed09047b
Tansu Dasli <tansudasli@gmail.com>
2020-03-21 19:10:42 +0300

    added /etc/hosts update step

---
+++ playbook-zookeeper/02a-ssh-passwordless.sh
@@ -0,0 +1,30 @@
+
+source ../.gcp.env
+source .env
+
+echo "switch to hadoop user!"
+
+# todo: ls -la ~/.ssh
+
+# add hostname and internal-IPs to /etc/hosts file
+echo "Add private-IPs into /etc/hosts file"
+
+for i in `gcloud compute instances list --project=${PROJECT_ID}| awk 'NR>1 {print $4 "#" $1}'`
+do
+  echo ${i} | tr '#' ' '| sudo tee -a /etc/hosts
+
+done
+
+
+# create ssh-keys
+echo "create ssh-keys, and update .ssh/authorized_keys"
+ssh-keygen -t rsa -P '' -f /home/hadoop/.ssh/id_rsa
+cat /home/hadoop/.ssh/id_rsa.pub >> /home/hadoop/.ssh/authorized_keys
+chmod 0600 /home/hadoop/.ssh/authorized_keys
+
+echo "\nRun 02a... file in all workers, then Manually copy /home/hadoop/.ssh/authorized_keys in master to the workers authorized_keys!"
+# send to other servers
+# - then in nameNode, copy .ssh/id_rsa.pub content into clipboard, and 
+# - ssh to machine-2 manually and add this into .ssh/authorized_keys content
+# - then conect w/ `ssh hadoop@machine-2` from machine-1 to machine-2
+# - do this to all machines where necessary
