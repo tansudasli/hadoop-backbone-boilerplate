e1ac59464e2382fadfa04177666cd749eac6af9a
Tansu Dasli <tansudasli@gmail.com>
2020-03-21 17:21:30 +0300

    added 2181 zk client conn. port

--- create-firewall-rule.sh
+++ create-firewall-rule.sh
@@ -9,7 +9,7 @@ gcloud compute --project=${PROJECT_NAME} firewall-rules create ${RULE_NAME} \
     --priority=1000 \
     --network=default \
     --action=ALLOW \
-    --rules=tcp:9870,tcp:9880 \
+    --rules=tcp:9870,tcp:9880,tcp:2181 \
     --source-ranges=0.0.0.0/0 \
     --target-tags=hadoop
 
