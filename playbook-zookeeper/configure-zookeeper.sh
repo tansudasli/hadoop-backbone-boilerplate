94873ede258568d0eb07bea77e93e169ceb9d20c
Tansu Dasli <tansudasli@gmail.com>
2020-03-21 17:20:53 +0300

    added myid file generation

--- playbook-zookeeper/configure-zookeeper.sh
+++ playbook-zookeeper/configure-zookeeper.sh
@@ -12,17 +12,35 @@ echo "run on znode only"
 # export NAME_NODE=${INSTANCE_NAMES[0]}
 # export SECONDARY_NAME_NODE=${INSTANCE_NAMES[1]}
 # export WORKER_NODES=(${INSTANCE_NAMES[2]} ${INSTANCE_NAMES[3]})
+
 export ZK_PATH=(/data-1)
 
 echo "configurations of ZK"
 
+echo "servername's format is critical. must be 6 char, and last one must be uniquenumber"
+hostname=`hostname`
+serverNumber=${x:5:1}
+
 
 # creates as new file
-# public-IP creates bindingException error
 cat > ${ZOOKEEPER_HOME}/conf/zoo.cfg <<EOL
 tickTime=2000
-dataDir=${ZK_PATH}/zookeeper
+dataDir=${ZK_PATH[0]}/zookeeper
+dataLogDir=${ZK_PATH[0]}/logs
 clientPort=2181
+initLimit=5
+syncLimit=2
+server.1=znode1:2888:3888
+server.2=znode2:2888:3888
+server.3=znode3:2888:3888
+server.4=znode4:2888:3888
+server.5=znode5:2888:3888
+EOL
+
+
+# todo: every server must have a different number
+cat > ${ZK_PATH[0]}/zookeeper/myid <<EOL
+$serverNumber
 EOL
 
 echo "run bin/zkServer.sh start, then check bin/zkCli.sh -server IP:2181 to enter zk-shell"
