9e29b62bcb23a80f3bbf7c735e6d47b8ed147b98
Tansu Dasli <tansudasli@gmail.com>
2020-03-22 22:52:31 +0300

    Initialize - added zk, hdfs for distributed mode

---
+++ playbook-hbase/configure-hadoop.sh
@@ -0,0 +1,95 @@
+
+source .env
+
+echo "run on nameNode only"
+#backups hadoop conf. files. then add new lines
+#then, formats hdfs namenode for the first time
+
+# contains hadoop specific env variables !
+
+# hadoop arch. topology - fully distributed
+export NAME_NODE=${INSTANCE_NAMES[0]}
+export SECONDARY_NAME_NODE=${INSTANCE_NAMES[1]}
+export WORKER_NODES=(${INSTANCE_NAMES[2]} ${INSTANCE_NAMES[3]})
+export HDFS_PATH=(/data-1)
+
+# STEP: backup
+echo "backup conf. files touched"
+cp ${HADOOP_HOME}/etc/hadoop/hadoop-env.sh ${HADOOP_HOME}/etc/hadoop/hadoop-env-backup.sh
+cp ${HADOOP_HOME}/etc/hadoop/core-site.xml ${HADOOP_HOME}/etc/hadoop/core-site-backup.xml
+cp ${HADOOP_HOME}/etc/hadoop/hdfs-site.xml ${HADOOP_HOME}/etc/hadoop/hdfs-site-backup.xml
+cp ${HADOOP_HOME}/etc/hadoop/workers ${HADOOP_HOME}/etc/hadoop/workers-backup
+
+# STEP: configuration
+echo "configurations of HDFS"
+
+echo export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java)))) >> ${HADOOP_HOME}/etc/hadoop/hadoop-env.sh
+echo export HADOOP_HOME=${HADOOP_HOME} >> ${HADOOP_HOME}/etc/hadoop/hadoop-env.sh
+echo export HADOOP_LOG_DIR=${HDFS_PATH}/logs >> ${HADOOP_HOME}/etc/hadoop/hadoop-env.sh
+
+
+cat > ${HADOOP_HOME}/etc/hadoop/core-site.xml <<EOL
+<configuration>
+    <property>
+        <name>fs.defaultFS</name>
+        <value>hdfs://${NAME_NODE}:9000</value>
+    </property>
+</configuration>
+EOL
+
+cat > ${HADOOP_HOME}/etc/hadoop/hdfs-site.xml <<EOL
+<configuration>
+
+<!--master-->
+    <property>
+            <name>dfs.namenode.name.dir</name>
+            <value>${HDFS_PATH[0]}/name</value>
+    </property>
+
+    <property>
+            <name>dfs.namenode.edits.dir</name>
+            <value>${HDFS_PATH[0]}/logs</value>
+    </property>
+
+    <property>
+            <name>dfs.namenode.secondary.http-address</name>
+            <value>${SECONDARY_NAME_NODE}:9880</value>
+    </property>
+
+    <property>
+        <name>dfs.replication</name>
+        <value>2</value>
+    </property>
+
+<!--secondaryMaster-->
+    <property>
+            <name>dfs.namenode.checkpoint.dir</name>
+            <value>${HDFS_PATH[0]}/secondaryName</value>
+    </property>
+
+<!--worker-->
+    <property>
+            <name>dfs.datanode.data.dir</name>
+            <value>${HDFS_PATH[0]}/data</value>
+    </property>
+</configuration>
+EOL
+
+# put only dataNodes' machines
+cat > ${HADOOP_HOME}/etc/hadoop/workers <<EOL
+${WORKER_NODES[0]}
+${WORKER_NODES[1]}
+EOL
+
+# STEP: distribute
+echo "distribute conf. files to all except master"
+
+for i in $(seq 1 1 $((${#INSTANCE_NAMES[@]}-1))) 
+do
+
+  scp ${HADOOP_HOME}/etc/hadoop/* hadoop@${INSTANCE_NAMES[i]}:${HADOOP_HOME}/etc/hadoop/
+done
+
+# STEP: format
+echo "format HDFS for the first time"
+${HADOOP_HOME}/bin/hdfs namenode -format
---
+++ playbook-hbase/configure-zookeeper.sh
@@ -0,0 +1,55 @@
+
+source .env
+
+
+echo "run on all znodes"
+#
+#
+
+# contains zk specific env variables !
+
+# design arch. topology - fully distributed
+export ZK_NODE=${INSTANCE_NAMES[@]:4:5]}
+export ZK_PATH=(/data-1)
+
+# STEP: configuration
+echo "configurations of ZK"
+
+echo "servername's format is critical. must be 6 char, and last one must be uniquenumber"
+hostname=`hostname`
+serverNumber=${hostname:5:1}
+echo "serverNumber="$serverNumber
+
+cat > ${ZOOKEEPER_HOME}/conf/zoo.cfg <<EOL
+tickTime=2000
+dataDir=${ZK_PATH[0]}/zookeeper
+dataLogDir=${ZK_PATH[0]}/logs
+clientPort=2181
+maxClientCnxns=60
+initLimit=5
+syncLimit=2
+server.1=${ZK_NODE[0]}:2888:3888
+server.2=${ZK_NODE[1]}:2888:3888
+server.3=${ZK_NODE[2]}:2888:3888
+server.4=${ZK_NODE[3]}:2888:3888
+server.5=${ZK_NODE[4]}:2888:3888
+EOL
+
+# create data dir, for myid file before start scripts.!
+mkdir ${ZK_PATH[0]}/zookeeper
+
+# todo: every server must have a different number
+cat > ${ZK_PATH[0]}/zookeeper/myid <<EOL
+$serverNumber
+EOL
+
+# STEP: create file to start zk easiliy
+cat > ${ZOOKEEPER_HOME}/start-zk.sh <<EOL
+cd ${ZOOKEEPER_HOME}
+nohup java -cp zookeeper.jar:lib/*:conf org.apache.zookeeper.server.quorum.QuorumPeerMain ./conf/zoo.cfg &
+EOL
+
+chmod +x ${ZOOKEEPER_HOME}/start-zk.sh
+
+echo "for cluster mode, run ${ZOOKEEPER_HOME}/start-zk.sh on all znodes"
+echo "then check client connection w/ -> ${ZOOKEEPER_HOME}/bin/zkCli.sh -server IP:2181 to enter zk-shell"
