447f33e64743da948f783a0a7052894272b2a349
Tansu Dasli <tansudasli@gmail.com>
2020-03-24 16:15:56 +0300

    added zk admin port

--- create-firewall-rule.sh
+++ create-firewall-rule.sh
@@ -9,7 +9,7 @@ gcloud compute --project=${PROJECT_ID} firewall-rules create ${RULE_NAME} \
     --priority=1000 \
     --network=default \
     --action=ALLOW \
-    --rules=tcp:9870,tcp:9880,tcp:2181,tcp:16010,tcp:16030 \
+    --rules=tcp:9870,tcp:9880,tcp:2181,tcp:16010,tcp:16030,tcp:8080 \
     --source-ranges=0.0.0.0/0 \
     --target-tags=hadoop
 
