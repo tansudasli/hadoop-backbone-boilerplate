b8c8ece8b8ada89028e8699ffda92aaca284c715
Tansu Dasli <tansudasli@gmail.com>
2020-03-24 16:28:20 +0300

    clarification increased and checked

--- create-gcp-project.sh
+++ create-gcp-project.sh
@@ -2,10 +2,11 @@
 
 source .gcp.env
 
-echo "First, create a gcp account and install gcp cli on your local"
+echo "First, create a gcp account and install gcp cli on your local, unless you use gclod shell"
 echo "Then, check active gcp account"
-echo "Then, edit .env file"
+echo "Then, edit .gcp.env file"
 echo "Then, run this script to create a project on gcp"
+echo "Then, run create-firewall-rule "
 
 gcloud projects create ${PROJECT_ID} --name ${PROJECT_NAME}
 
